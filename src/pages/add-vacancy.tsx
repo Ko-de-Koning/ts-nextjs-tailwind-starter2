926f84fc8d42ebb877d186d642456f4d70953621
Ko-de-Koning <110471206+Ko-de-Koning@users.noreply.github.com>
2022-11-18 14:59:53 +0100

    feat: vacancyobject working at landingpage

--- src/pages/add-vacancy.tsx
+++ src/pages/add-vacancy.tsx
@@ -21,14 +21,6 @@ const AddVacancy = async () => {
     getTasks();
   }, []);
 
-  // Fetch tasks
-  const fetchTasks = async () => {
-    const res = await fetch('http://localhost:5000/tasks');
-    const data = await res.json();
-
-    return data;
-  };
-
   // Add Task
   const addTask = async (task: VacancyObject) => {
     const res = await fetch('http://localhost:5000/tasks', {
